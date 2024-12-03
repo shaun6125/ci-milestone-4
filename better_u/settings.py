942e90b514768c3db257c59287d81c90012c2ab2
shaun craven <craven6622@googlemail.com>
2024-12-03 21:57:40 +0000

    added checkout app, forms.py, signals.py, views, admin, updated templates

--- better_u/settings.py
+++ better_u/settings.py
@@ -46,7 +46,9 @@ INSTALLED_APPS = [
     'home',
     'bag',
     'checkout',
-    
+
+    # Other
+    'crispy_forms',    
 ]
 
 MIDDLEWARE = [
@@ -62,6 +64,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'better_u.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -76,8 +80,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
