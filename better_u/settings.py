fb7e0f6282536b1371677648fc6d3e8b5f2797dc
shaun craven <craven6622@googlemail.com>
2024-12-11 06:05:06 +0000

    issue passing static files to aws

--- better_u/settings.py
+++ better_u/settings.py
@@ -198,7 +198,7 @@ if 'USE_AWS' in os.environ:
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    STATICFILES_LOCATION = 'staticfiles'
+    STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
